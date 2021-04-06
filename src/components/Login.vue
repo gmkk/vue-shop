85a73f7a9cb6da32512b675d457aa3feac08ded6
gmkk <1943801608@qq.com>
2021-04-06 20:32:01 +0800

    完成了登陆页面

---
+++ src/components/Login.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="login_container">
+       <div class="login_box">
+           <!-- 头像 -->
+           <div class="img_box">
+               <img src="../assets/logo.png" alt="">
+           </div>
+           <!-- 登录框 -->
+           <el-form label-width="0px" class="login_form" :model="loginform" :rules="loginformrules" ref="loginformRef">
+               <!-- 用户名 -->
+              <el-form-item prop="username">
+                <el-input  prefix-icon="iconfont icon-ziyuan" v-model="loginform.username"></el-input>
+              </el-form-item>
+              <!-- 密码 -->
+               <el-form-item prop="password" >
+                <el-input  prefix-icon="iconfont icon-suo" v-model="loginform.password" type="password"></el-input>
+              </el-form-item>
+              <!-- 按钮 -->
+               <el-form-item class="btns">
+                <el-button type="primary" @click="login">登录</el-button>
+                <el-button type="info" @click="resetfrom">重置</el-button>
+              </el-form-item>
+           </el-form>
+       </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            // 登录表单的数据绑定
+            loginform:{
+                username:"admin",
+                password:"123456"
+            },
+            loginformrules:{
+                // 用户名验证
+                 username:[
+                     { required: true, message: '请输入用户名', trigger: 'blur' },
+                     { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+                 ],
+
+                // 密码验证
+                password:[
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                    { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+                ]
+            }
+        }
+    },
+    methods:{
+        resetfrom(){
+            this.$refs.loginformRef.resetFields()
+        },
+        login(){
+            this.$refs.loginformRef.validate(async valid => {
+               if(!valid) return ;
+               const {data: res} = await this.$http.post('login',this.loginform)
+               if(res.meta.status !== 200) return this.$message.error("登陆失败");
+            this.$message.success("登陆成功")
+
+            window.sessionStorage.setItem("token",res.data.token);
+
+            this.$router.push("/home");
+            })
+        }
+    }
+    
+}
+</script>
+<style lang="scss" scoped>
+.login_container{background-color: #2b4b6b;height: 100%;}
+.login_box{width: 450px;height: 300px;background-color: white;border-radius: 3px;position: absolute;left: 50%;top: 50%;transform: translate(-50%,-50%);}
+.img_box{
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    padding: 10px;
+    border-radius: 50%;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    background-color: white;
+    box-shadow: 0 0 10px #ddd;
+    img{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+    }
+}
+.btns{
+    display: flex;
+    justify-content: flex-end;
+}
+.login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入图标
+import './assets/fonts/iconfont.css'
+// 导入element组件
+import Element from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Element)
+
+// 导入全局样式表
+import './assets/css/global.css'
+
+//配置请求的根路径
+import axios from 'axios'
+axios.defaults.baseURL = "http://127.0.0.1:8080/api/private/v1/"
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
